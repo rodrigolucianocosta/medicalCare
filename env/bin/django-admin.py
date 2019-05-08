ebe8e90505c9c85fe3b8da568d32965b9b2babd0
rodrigo luciano costa <rodrigolucianocosta@hotmail.com>
2019-05-08 09:56:47 -0300

    atualizaçao de configuraçoes alteraçao de linguagem para portugues configuraçao de ambiente

--- env/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/user/Documentos/OPENDEV/projetos/ProjetoHealthCareOpal101/env/bin/python3.6
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
